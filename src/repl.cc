88e51c08e3f7ba6d9b2a7ca6d8d8a2516f2ae1e5
Emil Edholm <bobby@verypublic.org>
2016-02-21 22:23:18 +0100

    Add cmd tab completion

--- src/repl.cc
+++ src/repl.cc
@@ -8,9 +8,44 @@
 #include "cmds.h"
 #include "repl.h"
 
+/*
+ * State is zero on first call, non-zero on for subsequent calls.
+ * Returns a NULL pointer to the caller when there are no more matches.
+ */
+char* cmd_generator(const char *txt, int state) {
+    static size_t length = 0;
+    static auto it       = cmd_map.cbegin();
+    std::string name;
+    if(state == 0) {
+        length = std::strlen(txt);
+        it = cmd_map.cbegin();
+    }
+
+    while(it != cmd_map.cend()) {
+        name = it->first;
+        it++;
+        if(name.compare(0, length, txt) == 0) {
+            return strdup(name.c_str());
+        }
+    }
+    return NULL;
+}
+
+/* Provide custom completion for readline */
+static char** auto_completion(const char* txt, int start, int end) {
+    // This prevents appending space to the end of the matching word
+    rl_completion_append_character = '\0';
+    char **matches = (char**)NULL;
+    if(start == 0) {
+        matches = rl_completion_matches(txt, &cmd_generator);
+    }
+    return matches;
+}
+
 ReadEvalPrint::ReadEvalPrint() {}
 
 void ReadEvalPrint::Loop() {
+    rl_attempted_completion_function = auto_completion;
     while (true) {
         char* cmd = Read();
         if(cmd == NULL) {
@@ -47,3 +82,4 @@ void ReadEvalPrint::Eval(char* cmd) {
         std::cout << "torrentsh: command not found: " << cmd << std::endl;
     }
 }
+
